683ff623406635e5a079bbd5ce20a68741c5d7a7
monginadiana <diana.ondieki@student.moringa.com>
2021-11-13 14:32:00 +0300

    creation of the profile html content

--- app/main/views.py
+++ app/main/views.py
@@ -2,24 +2,60 @@ from flask import render_template,request,redirect,url_for,abort,flash
 from . import main
 from ..models import User, Quote
 from flask_login import login_required,current_user
-from .. import db
+from .forms import UpdateProfile
+
+from .. import db,photos
 from ..requests import get_quote
+from flask.views import View,MethodView
 
 
 #views
 @main.route('/')
 def index():
+    
     '''
     View root page function that returns the index page and its data.
     '''
-    return render_template('index.html')
-
-@main.route('/quotes')
-def quotes():
-
-    '''
-    '''
     quote = get_quote()
-    title = 'LetsBlog | Quotes'
     
-    return render_template('quotes.html', title = title,quote = quote)
+    return render_template('index.html', quote = quote)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    title = f'{uname} Profile'
+    
+    return render_template("profile/profile.html", user = user, title = title)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+    title = 'Update | Profile'
+    return render_template('profile/update.html',form =form, title = title)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
